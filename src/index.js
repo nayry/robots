7b2ac110467fb8b98297ee504a39eda992ae78a2
Ryan <ryanwu1501@gmail.com>
2020-08-13 00:20:22 -0400

    adding redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { logger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import {searchRobots, requestRobots} from './reducers.js';
 import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({searchRobots, requestRobots});
+const store = createStore(rootReducer, 
+  applyMiddleware(thunkMiddleware,logger));
+
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
